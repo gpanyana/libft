ec9ba3dcb6a11ab3b10867d1aba332b336967ee9
Gcina PANYANA <gpanyana@c4r13s8.wethinkcode.co.za>
2019-05-20 18:43:19 +0200

    *Functions

--- Functions/ft_tolower.c
+++ Functions/ft_tolower.c
@@ -6,44 +6,22 @@
 /*   By: gpanyana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 17:13:34 by gpanyana          #+#    #+#             */
-/*   Updated: 2019/05/20 18:07:54 by gpanyana         ###   ########.fr       */
+/*   Updated: 2019/05/20 18:42:22 by gpanyana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putstr(char *str)
+char	ft_tolower(int c)
 {
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-		write(1, &str[i++], 1);
-}
-
-void	ft_tolower(int c)
-{
-	int i;
-	char lowercase;
-
-	i = 0;
-	if (str[i] >= 'a' && 'z' >= str[i])
-	{
-		lowercase = 0;
-		while (str[i] != '\0')
-		{
-			if (str[i] >= 'A' && 'Z' >= str[i])
-				lowercase = i += 32;
-		}
-		write(1, &lowercase, 1);
-		i += 1;
-	}
-	write(1, &str[i], 1);
-	return (0);
+	if ((c >= 97) && 122 >= c))
+		return (c);
+	if ((c >= 65) && ( >= c))
+		return (c + 32);
+	else 
+		return (0);
 }
 
 int		main(void)
 {
-	ft_tolower("HELLo");
-	return (0);
+	ft_tolower('G');
+	return (c);
 }
