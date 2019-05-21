684a0f3d82d3936a3325aa81e7026e9a5b9a5c4a
Gcina PANYANA <gpanyana@c4r11s11.wethinkcode.co.za>
2019-05-21 16:35:53 +0200

    added

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpanyana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 09:34:44 by gpanyana          #+#    #+#             */
+/*   Updated: 2019/05/21 16:35:04 by gpanyana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+void	ft_putchar(char c);
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
