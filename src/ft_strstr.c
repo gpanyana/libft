6d08c61d39f3e0858335d8c044e15c5c50e321b1
Event WeThinkCode <event@c4r11s11.wethinkcode.co.za>
2019-06-04 12:46:50 +0200

    Strstr

--- src/ft_strstr.c
+++ src/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: gpanyana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 14:12:36 by gpanyana          #+#    #+#             */
-/*   Updated: 2019/06/03 15:52:59 by gpanyana         ###   ########.fr       */
+/*   Updated: 2019/06/04 12:45:47 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,17 @@ char	*ft_strstr(const char *haystack, const char *needle)
 	size_t i;
 	size_t j;
 
+	if (needle[0] == '\0' && haystack[0] == '\0')
+		return ((char *)haystack);
 	i = 0;
-	if (needle == "")
-		return (haystack);
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)haystack + i);
+		i++;
+	}
+	return (NULL);
 }
