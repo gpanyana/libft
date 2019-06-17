4ea707477b6927c616e5aaede66cee2b07e2332f
Gcina PANYANA <gpanyana@c4r11s11.wethinkcode.co.za>
2019-06-17 15:57:17 +0200

    ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: gpanyana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/15 02:49:45 by gpanyana          #+#    #+#             */
-/*   Updated: 2019/06/15 05:21:46 by gpanyana         ###   ########.fr       */
+/*   Updated: 2019/06/17 15:51:50 by gpanyana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,22 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	j;
-	size_t	joint;
 	char	*fresh;
+	int		len;
 
+	fresh = NULL;
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	fresh = NULL;
-	if (!(fresh = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)) + 1)))
-		return (NULL);
-	joint = 0;
-	while (fresh[joint])
+	len = (ft_strlen(s1) + ft_strlen(s2));
+	if (s1 && s2)
 	{
-		fresh[joint] = ft_strcat(s1, s2);
-		joint++;
-	}
-	fresh[joint] != '\0';
-	return (fresh);
-}	
-		
-		/*{
-		i = 0;
-		while (s1[i] != '\0')
+		if (!(fresh = (char *)malloc(sizeof(char) * len + 1)))
+			return (NULL);
+		if (fresh)
 		{
-			fresh[joint] = s1[i];
-			joint++;
-			i++;
+			ft_strcpy(fresh, s1);
+			ft_strcat(fresh, s2);
 		}
-		j = 0;
-		while (s2[j] != '\0')
-		{
-			fresh[joint] = s2[j];
-			joint++;
-			j++;
-		}
-		fresh[joint + j] = '\0';
 	}
-//	fresh[joint] = '\0';
 	return (fresh);
+}
