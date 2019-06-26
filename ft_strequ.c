a4d45e38f8267b71532c4a108454791a0e3cb13a
Gcina PANYANA <gpanyana@c4r12s11.wethinkcode.co.za>
2019-06-26 17:36:44 +0200

    debugged equ

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: gpanyana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 12:35:43 by gpanyana          #+#    #+#             */
-/*   Updated: 2019/06/10 11:55:08 by gpanyana         ###   ########.fr       */
+/*   Updated: 2019/06/26 14:50:38 by gpanyana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,19 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
-	size_t	i;
 	int		k;
 
-	i = 0;
 	if (!(s1 && s2))
-		return (0);
-	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	while (*s1 != '\0' && *s2 != '\0')
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+	if (*s1 != '\0' && *s2 != '\0')
 	{
 		k = ft_strcmp((char *)s1, (char *)s2);
-		if (k == 0)
-			return (1);
-		else
+		if (k != 0)
 			return (0);
 	}
-	return (0);
+	return (1);
 }
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: gpanyana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 13:58:38 by gpanyana          #+#    #+#             */
-/*   Updated: 2019/06/07 15:41:18 by gpanyana         ###   ########.fr       */
+/*   Updated: 2019/06/26 15:09:20 by gpanyana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,19 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	size_t	i;
 	int		k;
 
-	i = 0;
 	if (!(s1 && s2))
 		return (0);
-	if (s1[0] == '\0' || s2[0] == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
-	while (*s1 != '\0' && *s2 != '\0' && i < n)
+	if (*s1 != '\0' && *s2 != '\0')
 	{
-		k = ft_strcmp((char *)s1, (char *)s2);
-		if (k == 0)
-			return (1);
-		else
+		k = ft_strncmp((char *)s1, (char *)s2, n);
+		if (k != 0)
 			return (0);
 	}
-	return (0);
+	return (1);
 }
